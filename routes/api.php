f8115bd14ee6e60f8861971b654b45807b2321d5
Miracle Chibuzo <miraclechibuzo@gmail.com>
2021-12-22 14:14:58 +0100

    .

--- routes/api.php
+++ routes/api.php
@@ -26,5 +26,10 @@ Route::middleware(['cors'])->group(function (){
     Route::post('submit_password_reset_token', [\App\Http\Controllers\API\AuthenticationController::class, 'submitPasswordResetToken']);
 });
 Route::middleware(['auth:api', 'cors'])->group(function (){
-
+    Route::get('auth', function (){
+        return response([
+            'status'=>true,
+            'message'=>'you are in'
+        ]);
+    });
 });
