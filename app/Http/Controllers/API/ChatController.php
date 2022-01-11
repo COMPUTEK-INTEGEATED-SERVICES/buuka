33f7879b4d09663b48097e04f34b67f1eeebeaa3
Miracle Chibuzo <miraclechibuzo@gmail.com>
2022-01-11 08:06:32 +0100

    fixed all messages endpoint

--- app/Http/Controllers/API/ChatController.php
+++ app/Http/Controllers/API/ChatController.php
@@ -173,7 +173,7 @@ class ChatController extends Controller
         $chat =  Chat::with(['user_1', 'user_2'])->select(DB::raw('t.*'))
             ->from(DB::raw('(SELECT * FROM chats ORDER BY created_at DESC) t'))
             ->where('user_2', $this->user->id)
-            ->groupBy('t.user_1')
+            ->groupBy('t.user_2')
             ->get();
 
         return response([
