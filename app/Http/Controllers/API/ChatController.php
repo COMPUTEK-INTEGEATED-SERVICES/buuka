00aec1cbe1133bc1018925d75a3e662bf7443c89
Miracle Chibuzo <miraclechibuzo@gmail.com>
2022-01-11 08:24:22 +0100

    fixed all messages endpoint

--- app/Http/Controllers/API/ChatController.php
+++ app/Http/Controllers/API/ChatController.php
@@ -168,7 +168,7 @@ class ChatController extends Controller
 
     public function getAllMessages()
     {
-        $chat = Chat::with(['user_1', 'user_2'])->where('user_2', $this->user->id)
+        $chat = Chat::where('user_2', $this->user->id)
             ->orderBy('created_at')->paginate(10);
         /*$chat =  Chat::select(DB::raw('t.*'))
             ->from(DB::raw('(SELECT * FROM chats ORDER BY created_at DESC) t'))
