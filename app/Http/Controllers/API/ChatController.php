2cd54f8d4c6aa1d32c234dd32c629e8ecc410f26
Miracle Chibuzo <miraclechibuzo@gmail.com>
2022-01-11 07:53:07 +0100

    fixed all messages endpoint

--- app/Http/Controllers/API/ChatController.php
+++ app/Http/Controllers/API/ChatController.php
@@ -167,7 +167,8 @@ class ChatController extends Controller
 
     public function getAllMessages()
     {
-        $chat = Chat::with(['user_1', 'user_2'])->where('user_2', $this->user->id)->latest()->groupBy('chats.user_1')->paginate(10);
+        $chat = Chat::with(['user_1', 'user_2'])->where('user_2', $this->user->id)
+            ->orderBy('chats.id')->groupBy('chats.user_1')->paginate(10);
 
         return response([
             'status'=>true,
