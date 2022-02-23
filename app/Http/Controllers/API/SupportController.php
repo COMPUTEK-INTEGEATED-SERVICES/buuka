ce6ff6ce1491c6386eb0c37223725166bd1483d4
Miracle Chibuzo <miraclechibuzo@gmail.com>
2022-02-23 05:10:23 +0100

    code bump

--- app/Http/Controllers/API/SupportController.php
+++ app/Http/Controllers/API/SupportController.php
@@ -466,7 +466,7 @@ class SupportController
 
     public function getServices(Request $request)
     {
-        $services = Service::with([])
+        $services = Service::with(['vendor', 'products'])
             ->leftJoin('vendors', 'services.vendor_id', '=', 'vendors.id')
             ->leftJoin('products', 'products.service_id', '=', 'services.id')->with(['resources'])
             ->leftJoin('category_relations', 'category_relations.relateable_id', '=', 'vendors.id')
