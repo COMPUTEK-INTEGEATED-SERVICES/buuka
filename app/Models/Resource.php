1e46d0dbbcb09cce6425d821706e78e9d8352f2c
Miracle Chibuzo <miraclechibuzo@gmail.com>
2022-02-24 18:41:54 +0100

    bug fix

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -13,6 +13,9 @@ class Resource extends Model
         'path', 'resourceable_id', 'resourceable_type'
     ];
 
+    protected $hidden = [
+        'id', 'resourceable_id', 'resourceable_type'
+    ];
     public function resourceable(): \Illuminate\Database\Eloquent\Relations\MorphTo
     {
         return $this->morphTo();
