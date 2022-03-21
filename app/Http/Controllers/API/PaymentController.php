ae66b15ff9bb9feecfd3d7c718a74f2b89e8d66e
Miracle Chibuzo <miraclechibuzo@gmail.com>
2022-03-21 13:56:12 +0100

    bug fix

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -436,7 +436,7 @@ class PaymentController extends \App\Http\Controllers\Controller
                 $book = Book::find(TransactionReference::where('reference', $data->tx_ref)
                     ->where('referenceable_type', 'App\Models\Book')->first()->referenceable_id);
 
-                if($data->amount == $book->amount && $data->currency == 'NGN' && (new OrderController())->completeOrder($book->id))
+                if($data->amount > $book->amount && $data->currency == 'NGN' && (new OrderController())->completeOrder($book->id))
                 {
                     return response([
                         'status'=>true,
