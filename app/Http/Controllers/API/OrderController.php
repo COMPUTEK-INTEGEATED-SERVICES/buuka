3fea43f8fa4052d06dd2a32177ba43322a62c09f
Miracle Chibuzo <miraclechibuzo@gmail.com>
2022-03-21 19:43:01 +0100

    bug fix

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -407,7 +407,7 @@ class OrderController extends Controller
             ]);
         }
 
-        $books = Book::with(['reference', 'vendor'])
+        $books = Book::with(['reference', 'vendor', 'user'])
             ->where('user_id', $this->user->id)
             ->where(function ($query) use ($request) {
                 $flag = NULL;
