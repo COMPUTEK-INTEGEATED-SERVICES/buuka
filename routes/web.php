74f73071e37014a0e90b55f89d29cc116c7e82b6
Miracle Chibuzo <miraclechibuzo@gmail.com>
2022-03-24 19:33:32 +0100

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -19,20 +19,5 @@ Route::get('/', function () {
 });
 
 Route::get('test' ,function (){
-    $cities = json_decode(file_get_contents('./extras/cities_in_nigeria.json'));
-    $country = \App\Models\Country::where('name', 'Nigeria')->where('initial', 'NGA')->first();
-    foreach ($cities as $items)
-    {
-        $state = \App\Models\State::where('country_id', $country->id)->where('name', $items->name)->first();
-        if ($state)
-        {
-            $citiez = $items->cities;
-            foreach ($citiez as $city){
-                \App\Models\City::create([
-                    'state_id'=>$state->id,
-                    'name'=>$city
-                ]);
-            }
-        }
-    }
+
 });
