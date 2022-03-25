3358435d24034f59c2d3b92b6f1502773756ce20
Miracle Chibuzo <miraclechibuzo@gmail.com>
2022-03-25 14:46:04 +0100

    wallet fund

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -70,22 +70,30 @@ class UserController extends Controller
             ], 422);
         }
 
-        $user = User::find($this->user->id);
-        $user->first_name = $request->input('first_name', $user->first_name);
-        $user->last_name = $request->input('last_name', $user->last_name);
-        $user->email = $request->input('email', $user->email);
-        $user->email_verified = (!empty($request->email) && $user->email != $request->email)?0:$user->email_verified;
-        $user->phone = $request->input('phone', $user->phone);
-        $user->phone_verified = (!empty($request->phone) && $user->phone != $request->phone)?0:$user->phone_verified;
-        $user->gender = $request->input('gender', $user->gender);
-        $user->date_of_birth = $request->input('date_of_birth', $user->date_of_birth);
-        $user->save();
+        try {
+            $user = User::find($this->user->id);
+            $user->first_name = $request->input('first_name', $user->first_name);
+            $user->last_name = $request->input('last_name', $user->last_name);
+            $user->email = $request->input('email', $user->email);
+            $user->email_verified = (!empty($request->email) && $user->email != $request->email)?0:$user->email_verified;
+            $user->phone = $request->input('phone', $user->phone);
+            $user->phone_verified = (!empty($request->phone) && $user->phone != $request->phone)?0:$user->phone_verified;
+            $user->gender = $request->input('gender', $user->gender);
+            $user->date_of_birth = $request->input('date_of_birth', $user->date_of_birth);
+            $user->save();
 
-        return response()->json([
-            'status' => true,
-            'message' => 'Profile Saved',
-            'data' => []
-        ]);
+            return response()->json([
+                'status' => true,
+                'message' => 'Profile Saved',
+                'data' => []
+            ]);
+        }catch (\Throwable $throwable){
+            return response()->json([
+                'status' => false,
+                'message' => 'An error occurred',
+                'data' => []
+            ]);
+        }
     }
 
     public function userLastSeen()
