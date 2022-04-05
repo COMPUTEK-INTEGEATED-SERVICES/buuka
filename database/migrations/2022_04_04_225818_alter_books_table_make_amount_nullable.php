ec545890e93ca64919f0dffd29e78378a878a461
Miracle Chibuzo <miraclechibuzo@gmail.com>
2022-04-05 07:58:11 +0100

    bugfix - altered book created a ProductBookRelation for book to match products

--- database/migrations/2022_04_04_225818_alter_books_table_make_amount_nullable.php
+++ database/migrations/2022_04_04_225818_alter_books_table_make_amount_nullable.php
@@ -14,7 +14,7 @@ class AlterBooksTableMakeAmountNullable extends Migration
     public function up()
     {
         Schema::table('books', function (Blueprint $table) {
-            //
+            $table->string('amount')->nullable()->change();
         });
     }
 
