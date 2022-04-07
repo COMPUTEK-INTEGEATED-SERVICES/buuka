037e8c8821bf4fa2b9a6867c2d359f27bf82ea2c
Miracle Chibuzo <miraclechibuzo@gmail.com>
2022-04-07 20:24:12 +0100

    pusher beam push notification setup - changed package

--- app/Notifications/Order/UserBookSuccessfulNotification.php
+++ app/Notifications/Order/UserBookSuccessfulNotification.php
@@ -33,7 +33,7 @@ class UserBookSuccessfulNotification extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail', 'database', PusherChannel::class];
+        return ['mail', 'database'];
     }
 
     /**
