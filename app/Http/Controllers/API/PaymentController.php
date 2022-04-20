b115081b8e25f6aebdc059ab201e8f4c948c03d8
Miracle Chibuzo <miraclechibuzo@gmail.com>
2022-04-20 03:52:37 +0100

    vendor withdrawal feature - added account resolve endpoint

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -670,7 +670,7 @@ class PaymentController extends \App\Http\Controllers\Controller
         ];
         $response = Http::withToken(env('FLW_SECRET_KEY'))
             ->post($url, $field);
-        if ($response->ok() && $response->json()['status'] ==='success'){
+        if ($response->ok() && $response->json()['status'] =='success'){
             return response()->json([
                 'status' => true,
                 'message' => 'Account details',
