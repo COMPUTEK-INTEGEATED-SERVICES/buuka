6cea2f5b0c04df8b5eadd44817b7318c1567f581
Miracle Chibuzo <miraclechibuzo@gmail.com>
2022-04-26 14:06:10 +0100

    bugfix - refactored the store withdrawal method to accommodate withdrawal for both user and vendor

--- app/Http/Controllers/API/InitController.php
+++ app/Http/Controllers/API/InitController.php
@@ -31,7 +31,7 @@ class InitController extends Controller
                 'bank_accounts'=>$this->user->accounts,
                 'vendor'=>[
                     'is_vendor'=> boolval((Vendor::where('user_id', $this->user->id)->first())?1:0),
-                    'vendor'=> Vendor::with('accounts')->where('user_id', $this->user->id)->first()??null,
+                    'vendor'=> Vendor::with(['accounts', 'wallet'])->where('user_id', $this->user->id)->first()??null,
                 ],
                'admin'=>[
                    'is_admin'=>$this->user->hasRole('admin'),
