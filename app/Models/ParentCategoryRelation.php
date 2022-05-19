0b999475922e3fd4801cc41c860362b73dd13a1a
Miracle Chibuzo <miraclechibuzo@gmail.com>
2022-05-19 23:50:44 +0100

    bugfix

--- app/Models/ParentCategoryRelation.php
+++ app/Models/ParentCategoryRelation.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class ParentCategoryRelation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'parent_id', 'category_id'
+    ];
 }
